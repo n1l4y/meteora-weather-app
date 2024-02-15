8b79f063061167759d74373bb7b8953b8253d025
Nilay Shah <nilayshah630@gmail.com>
2024-02-16 01:04:34 +0530

    switch to HTTPS

--- assets/js/api.js
+++ assets/js/api.js
@@ -29,13 +29,13 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
   },
   reverseGeo(lat, lon) {
-    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
   },
   /**
    *
    * @param {string} query Search query
    */
   geo(query) {
-    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
   },
 };
